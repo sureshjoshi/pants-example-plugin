53a787775163438d929f59ecc38d1910062e9a0f
SJ <suresh@vicarasolutions.com>
2022-01-24 14:01:16 -0500

    Registering the FooifyTarget

--- pants-plugins/experimental/fooify/register.py
+++ pants-plugins/experimental/fooify/register.py
@@ -4,4 +4,16 @@ def rules():
 
 # This registers our new plugin's target(s) (used in BUILD files)
 def target_types():
-    return []
+    return [FooifyTarget]
+
+from pants.engine.target import (
+    COMMON_TARGET_FIELDS,
+    Target,
+)
+
+class FooifyTarget(Target):
+    alias = "fooify"
+    core_fields = (
+        *COMMON_TARGET_FIELDS,
+    )
+    help = "The `fooify` target will take in a wheel dependency and add a .foo extension to the end."
