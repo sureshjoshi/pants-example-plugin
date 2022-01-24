0f92f68b98b4e045febca7a0c710464162091cf8
SJ <sureshj@gmail.com>
2022-01-24 14:30:26 -0500

    Added deps to the Fooify target

--- pants-plugins/experimental/fooify/target_types.py
+++ pants-plugins/experimental/fooify/target_types.py
@@ -1,5 +1,6 @@
 from pants.engine.target import (
     COMMON_TARGET_FIELDS,
+    Dependencies,
     Target,
 )
 
@@ -7,5 +8,6 @@ class FooifyTarget(Target):
     alias = "fooify"
     core_fields = (
         *COMMON_TARGET_FIELDS,
+        Dependencies,
     )
     help = "The `fooify` target will take in a wheel dependency and add a .foo extension to the end."
